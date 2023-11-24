01c0aa1ee19812275ce406ba715cd6fc899bc776
Sergio <sergio.moreno@tajamar365.com>
2023-11-24 20:53:07 +0100

    CourseappReactEmpezado

--- laboratoriosReact/courseapp/src/Editor.js
+++ laboratoriosReact/courseapp/src/Editor.js
@@ -1,14 +1,31 @@
 import React, { Component } from "react";
 
-
 export class Editor extends Component {
-
     constructor(props) {
         super(props);
         this.state = {
-            title: "",
-            seatCapacity: "",
-            instructorName: ""
-        }
+            name: ""
+        };
+    }
+
+    updateFormValue = (event) => {
+        this.setState({ [event.target.name]: event.target.value },
+            () => this.props.submit(this.state));
+    }
+
+    render() {
+        return (
+            <div className="h5 bg-info text-white p-2">
+                <div className="form-group">
+                    <label>Name:</label>
+                    <input
+                        className="form-control"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.updateFormValue}
+                    />
+                </div>
+            </div>
+        );
     }
 }
